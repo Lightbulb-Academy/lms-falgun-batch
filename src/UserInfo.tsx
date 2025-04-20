23587d3fe2aefbecbf41750f26577b5dfb19298a
niteshsingh1357 <niteshsingh1357@gmail.com>
2025-04-20 15:07:01 +0545

    form handling

--- src/UserInfo.tsx
+++ src/UserInfo.tsx
@@ -1,23 +1,13 @@
-import React, { FormEvent, useState } from "react";
+import { FormEvent, useState } from "react";
 
 export default function UserInfo() {
-  const [name, setName] = useState("John Doe");
-  const [address, setAddress] = useState("Mahendra Chowk, Brt");
-  const [phone, setPhone] = useState("987888989");
-
-  const [userInfo, setUserInfo] = useState({
-    name: "John Doe",
-    address: "asdada, adsada",
-    phone: "887988798",
-  });
-
   const [submittedUserInfo, setSubmittedUserInfo] = useState({
     name: "John Doe",
     address: "asdada, adsada",
     phone: "887988798",
   });
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = (e: FormEvent) => {
     e.preventDefault(); // prevents page reload on form submit
     console.log(e);
     setSubmittedUserInfo({
@@ -25,7 +15,18 @@ export default function UserInfo() {
       address: e.target["address"].value,
       phone: e.target["phone"].value,
     });
+    // const formData = new FormData(e.target)
+    //
+    // API call
   };
+
+  // Events
+  // - onChange
+  // - onSubmit
+  // - onClick
+  // - onKeyPress
+
+  // next class: useEffect
 
   return (
     <div className="flex flex-col gap-8">
@@ -36,45 +37,11 @@ export default function UserInfo() {
       </div>
       <form className="flex flex-col items-start gap-2" onSubmit={handleSubmit}>
         <label htmlFor="name">Name: </label>
-        <input
-          id="name"
-          type="text"
-          className="border px-2 w-full"
-          // value={userInfo.name}
-          // onChange={(e) =>
-          //   setUserInfo((prevUserInfo) => ({
-          //     // prevUserInfo gives current state value
-          //     ...prevUserInfo, // copy of state value
-          //     name: e.target.value,
-          //   }))
-          // } // onChange is an input event
-        />
+        <input id="name" type="text" className="border px-2 w-full" />
         <label htmlFor="address">Address: </label>
-        <input
-          id="address"
-          type="text"
-          className="border px-2 w-full"
-          // value={userInfo.address}
-          // onChange={(e) =>
-          //   setUserInfo((prevUserInfo) => ({
-          //     ...prevUserInfo,
-          //     address: e.target.value,
-          //   }))
-          // } // onChange is an input event
-        />
-        {/* Add label and input element for address and phone */}
+        <input id="address" type="text" className="border px-2 w-full" />
         <label htmlFor="phone">Phone: </label>
-        <input
-          id="phone"
-          type="text"
-          className="border px-2 w-full"
-          // onChange={(e) =>
-          //   setUserInfo((prevUserInfo) => ({
-          //     ...prevUserInfo,
-          //     phone: e.target.value,
-          //   }))
-          // } // onChange is an input event
-        />
+        <input id="phone" type="text" className="border px-2 w-full" />
         <button
           type="submit"
           className="w-full bg-amber-600 text-white py-1 mt-4"
